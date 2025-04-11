b5c82aaf8be3597d41fe148bc55c0b67e7de7487
silaswpo <silaswesleypereiradeoliveira@gmail.com>
2025-04-11 16:15:48 -0300

    Codigo co duas possibilidades de solução novamente

--- manipular_vetores.v
+++ manipular_vetores.v
@@ -1,8 +1,8 @@
 module manipular_vetores( 
     input [31:0] entrada,
-    output [31:0] saida );
+    output reg [31:0] saida );
 
-   /* always @(*)begin
+    always @(*)begin
         //[0:7];[8:15];[16:23];[24:31] 
 
         saida[7:0] = entrada[31:24];
@@ -10,9 +10,9 @@ module manipular_vetores(
         saida[23:16] = entrada[15:8];
         saida[31:24] = entrada[7:0];
 
-    end */
+    end 
 
-
-
-assign saida={entrada[7:0],entrada[15:8],entrada[23:16],entrada[31:24] };
 endmodule
+
+/*assign saida={entrada[7:0],entrada[15:8],entrada[23:16],entrada[31:24] };
+endmodule*/
